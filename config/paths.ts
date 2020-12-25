d4c11907d3eb45661ec6f5048ba20a5b30dcee24
beto <josereyes55@hotmail.com>
2020-12-24 18:32:17 -0600

    adding module that are going to be use to resolve dependencies by webpack

--- config/paths.ts
+++ config/paths.ts
@@ -4,9 +4,17 @@ import fs from 'fs';
 const appRootPath = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath: string) => resolve(appRootPath, relativePath);
 
-const paths: Record<string, string> = {
+const paths: Record<string, string | string[]> = {
     src: resolveApp('src'),
-    serverBuild: resolveApp('build/server')
+    serverBuild: resolveApp('build/server'),
+    srcServer: resolveApp('src/server'),
+    publicPath: '/static/'
 };
+
+paths.resolveModules = [
+    paths.srcServer as string,
+    paths.src as string,
+    'node_modules'
+];
 
 export default paths;
