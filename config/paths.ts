da6f1e52aa0b1255742dc389c7b5c83d7af34ac1
beto <josereyes55@hotmail.com>
2020-12-26 11:15:13 -0600

    adding build and src paths

--- config/paths.ts
+++ config/paths.ts
@@ -2,18 +2,26 @@ import { resolve } from 'path';
 import fs from 'fs';
 
 const appRootPath = fs.realpathSync(process.cwd());
-const resolveApp = (relativePath: string) => resolve(appRootPath, relativePath);
+const resolveApp = (relativePath: string): string => resolve(appRootPath, relativePath);
 
 const paths: Record<string, string | string[]> = {
+    appHtml: resolveApp('config/webpack.config.ts/template.html'),
     src: resolveApp('src'),
+    config: resolveApp('config'),
     serverBuild: resolveApp('build/server'),
+    clientBuild: resolveApp('build/client'),
     srcServer: resolveApp('src/server'),
+    srcShared: resolveApp('src/shared'),
+    srcClient: resolveApp('src/client'),
     publicPath: '/static/'
 };
 
 paths.resolveModules = [
+    paths.srcClient as string,
     paths.srcServer as string,
+    paths.srcShared as string,
     paths.src as string,
+    paths.config as string,
     'node_modules'
 ];
 
